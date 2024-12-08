084089b33633cd2968dd940f0c6d95463ee5d185
shawa <shawa@localhost>
2024-12-08 12:42:58 +0000

    7 attempt part 2

--- lib/advent_2024/day_07.ex
+++ lib/advent_2024/day_07.ex
@@ -1,55 +1,74 @@
 defmodule Advent2024.Day07 do
   use ExAdvent.Day, day: 7, input: &Advent2024.Day07.Parser.parse/1
-  # @operators [&Kernel.+/2, &Kernel.*/2]
-  def part_one(equations) do
+
+  def part_one(input) do
+    solve(input, 1)
+  end
+
+  def part_two(input) do
+    solve(input, 2)
+  end
+
+  def operators_for(1), do: [&Kernel.+/2, &Kernel.*/2]
+
+  def operators_for(2) do
+    [(&:erlang.binary_to_integer("#{&1}#{&2}")) | operators_for(1)]
+  end
+
+  def solve(equations, part) do
+    operators = operators_for(part)
+
     equations
-    |> Enum.filter(&valid/1)
-    |> Enum.map(fn {value, _xs} -> value end)
+    |> Task.async_stream(&{&1, valid(&1, operators)})
+    |> Stream.map(fn
+      {
+        :ok,
+        {{target, _terms}, valid}
+      } ->
+        {target, valid}
+    end)
+    |> Enum.filter(fn {_target, valid} -> valid end)
+    |> Enum.map(fn {target, _valid} -> target end)
     |> Enum.sum()
   end
 
-  def build(xs, ops) do
-    build_(Enum.reverse(xs), Enum.reverse(ops))
-  end
-
-  def valid({target, xs}) do
-    number_ops = length(xs) - 1
-    candidate_operators = [&Kernel.+/2, &Kernel.*/2]
-
-    ops = generate(number_ops, candidate_operators)
+  def valid({target, xs}, candidate_operators) do
+    ops = generate(length(xs) - 1, candidate_operators)
 
     ops
-    |> Enum.map(&build(xs, &1))
+    |> Enum.map(&interzip(xs, &1))
     |> Enum.map(&eval/1)
     |> Enum.any?(&(&1 == target))
   end
 
-  def build_([x, y], [op]), do: {op, [x, y]}
-  def build_([x | xs], [op | ops]), do: {op, [x, build_(xs, ops)]}
-
-  def eval({fun, [x, y]}), do: fun.(eval(x), eval(y))
-  def eval(x) when is_integer(x), do: x
+  def eval([n]) when is_integer(n), do: n
+  def eval([x, fun, y | rest]), do: eval([fun.(x, y) | rest])
+  def interzip([x], []), do: [x]
+  def interzip([x | xs], [fun | funs]), do: [x, fun | interzip(xs, funs)]
 
   def generate(n, operators) do
-    base = length(operators)
-
     operator_map =
-      operators |> Enum.with_index() |> Map.new(fn {op, i} -> {i, op} end)
+      Map.new(
+        Enum.with_index(operators),
+        fn {op, i} -> {i, op} end
+      )
 
-    target =
-      for _ <- 1..n do
-        "#{base - 1}"
-      end
-      |> Enum.join()
-      |> :erlang.binary_to_integer(2)
+    base = length(operators)
+    target = trunc(:math.pow(base, n) - 1)
 
     for i <- 0..target do
       i
-      |> Integer.to_string(2)
-      |> String.pad_leading(n, "0")
-      |> String.to_charlist()
-      |> Enum.map(&(&1 - ?0))
-      |> Enum.map(&Map.fetch!(operator_map, &1))
+      |> Integer.to_charlist(base)
+      |> pad_leading(n)
+      |> Enum.map(&Map.fetch!(operator_map, &1 - ?0))
     end
   end
+
+  def pad_leading(xs, n) do
+    diff = max(n - length(xs), 0)
+    pad(xs, diff)
+  end
+
+  def pad(xs, 0), do: xs
+  def pad(xs, n), do: pad([?0 | xs], n - 1)
 end
